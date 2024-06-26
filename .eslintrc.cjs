b73647add2c7b67a19b2867e3608769e9e7d659a
Nadelopo <mistrmorsikk@mail.ru>
2024-06-26 13:54:16 +0400

    configured project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    '@vue/eslint-config-airbnb',
   ],
+  ignores: ['vite.config.ts'],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+};
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import App from '../App.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,9 +7,9 @@ const router = createRouter({
     {
       path: '/',
       name: 'Home',
-      component: () => import('../App.vue')
-    }
-  ]
-})
+      component: App,
+    },
+  ],
+});
 
-export default router
+export default router;
