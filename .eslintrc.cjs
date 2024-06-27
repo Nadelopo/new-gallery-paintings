fb509d1c7fc13d203b54f69e31ce50f07a8a110f
Nadelopo <mistrmorsikk@mail.ru>
2024-06-27 20:58:20 +0400

    chore: eslint tsconfig

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,30 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
+const path = require('node:path');
+const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting');
 
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting',
     '@vue/eslint-config-airbnb',
   ],
-  ignores: ['vite.config.ts'],
+  ignorePatterns: ['vite.config.ts'],
   parserOptions: {
     ecmaVersion: 'latest',
+  },
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'implicit-arrow-linebreak': 'off',
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`,
+    }),
   },
 };
